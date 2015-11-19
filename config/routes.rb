60e1e8403ad6f36fedb22c9f904bf0be1b801d55
jacobleesinger <jacobleesinger@gmail.com>
2015-11-19 14:43:31 -0800

    add bands/albums/tracks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
    resources :users, only: [:create, :new, :show]
    resource :session, only: [:create, :destroy, :new]
+
+   resources :bands
+   resources :albums
+   resources :tracks
 end
